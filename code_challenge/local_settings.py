1b045c6bc49a1bed5183116db13ce8c9efa5eb6c
ernestwang <erzhuo.wang88@gmail.com>
2014-09-04 15:59:08 -0400

    force adding local_settings

---
+++ code_challenge/local_settings.py
@@ -0,0 +1,15 @@
+import os
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'foodtrucks',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
